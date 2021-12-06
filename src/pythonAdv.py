c41111d453a8e0772278bd1656a785418fe9f56a
Derek Todd <derekhtodd@yahoo.co.uk>
2021-12-06 10:05:00 +0000

    some more translating work

--- src/pythonAdv.py
+++ src/pythonAdv.py
@@ -44,9 +44,26 @@ def main():
     # initialise the state variables
     gameState, commandsState = initDataStructs()
     
+    keepGoing = True
+
+    while (keepGoing):
+
+        userCommands = {}
+        userInstruction = ""
+
+        userCommands = parseEnteredCommand()
+        userInstruction = processParsedCommand(userCommands)
+        processHighLevelInstruction(userInstruction)
 
 
 
+        # This was the JS callback to read the 'messages' table in from the ostgres server side database - for this project we'll dump the data into a JSON which gets read into memory (possibly the gameState object, but may be better as an object on its own?
+        # classicGetMessages(function(classicGetMessages) {
+        # classicGameStatus.classicMessages = classicGetMessages;
+        # });
+
+        # because of the way the code flowed in the JS version, an output was built up and then the updateDescription() function was called. 
+        updateDescription(gameState)
 
 
 
